ebc99d318172396c35df8005159260168d5d3274
David Aguayo <david.aguayo@gmail.com>
2019-08-22 14:42:10 -0400

    Create Open_csv.py

---
+++ Open_csv.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import csv
+
+with open('data_football.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
