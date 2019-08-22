08911b7f5f7e360824caaf6ea39c6fc7110bff78
David Aguayo <david.aguayo@gmail.com>
2019-08-22 14:53:05 -0400

    Update Open_csv.py

--- Open_csv.py
+++ Open_csv.py
@@ -5,3 +5,4 @@ with open('data_football.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         print(', '.join(row))
+
